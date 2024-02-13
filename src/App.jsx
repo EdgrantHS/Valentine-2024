b2b8281bbec7c80bae6fd4e5bb4ba86e0ee31168
EdgrantHS <edgrant.h@gmail.com>
2024-02-13 23:47:18 +0700

    angpao jadi gradient

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
 
   return (
     <>
-      <div id='red_anvelope' className='bg-red-500 w-[300px] h-[500px] outline outline-4 outline-red-600 rounded-md relative shadow-lg' onClick={handleToggleLetter}>
+      <div id='red_anvelope' className='from-red-400 to-red-600 bg-gradient-to-tr w-[300px] h-[500px] outline outline-4 outline-red-700 rounded-md relative shadow-lg' onClick={handleToggleLetter}>
         <div id="anvelope-white" className="w-full h-1/5 bg-gray-50 rounded-b-3xl absolute flex justify-center items-end">
           <div id="anvelope-white" className="w-4/5 h-3/4 bg-gray-200 border-gray-300 border-t-4 border-x-4 overflow-hidden rounded-t-md text-black flex-col flex items-center p-4">
             <h1 className='text-2xl'>Letter</h1>
           </div>
         </div>        
-        <div id="anvelope-flap" className="w-full h-1/5 outline outline-4 outline-red-600 rounded-b-3xl bg-rose-500 absolute" />       
+        <div id="anvelope-flap" className="w-full h-1/5 outline outline-4 outline-red-600 rounded-b-3xl from-rose-500 to-rose-600 bg-gradient-to-bl absolute" />       
         <div className="flex justify-center items-center w-full">
           <div id="dragon" className="w-[200px] h-[200px] absolute bottom-1/4 bg-yellow-300 rounded-full flex justify-center items-center border-4 border-yellow-500">
             <img src="https://www.svgrepo.com/show/493833/dragon.svg" alt="dragon" className='h-3/5 w-3/5' />
@@ -28,9 +28,9 @@ function App() {
         <h1 className='text-4xl py-4'>Letter</h1>
         <div className="w-full h-4/5 text-xl p-4 flex flex-col items-center">
           <p className='text-center'>
-            Helo Leciiiiiiiii, Happy Valentines day. Sedih banget gak bisa ketemu gara gara kamu di bangka. Aku harap kamu suka website ini YAY.
+            Helo Leciiiiiiiii, Happy Valentines day. Sedih banget gak bisa ketemu gara gara kamu di bangka. Aku harap kamu suka website ini YAY. Happy CNY Juga!!!
           </p>
-        <div id="dragon" className="w-[200px] h-[200px] absolute bottom-1/4 bg-rose-300 rounded-full flex justify-center items-center border-4 border-rose-500 -mb-8">
+        <div id="dragon" className="w-[200px] h-[200px] absolute bottom-1/4 bg-rose-300 rounded-full flex justify-center items-center border-4 border-rose-500 -mb-14">
             <img src="https://www.svgrepo.com/show/532473/heart.svg" alt="heart" className='h-3/5 w-3/5' />
           </div> 
 
